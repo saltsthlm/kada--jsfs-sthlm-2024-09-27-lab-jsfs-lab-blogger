9b139dc403f65a7459e11b57597b4c68fa71215d
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-29 12:07:47 +0100

    made-app-display-the-.md-files-as-html-files
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,42 @@
-import express from "express";
-import { Request, Response } from "express";
+// app.ts
+import express, { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import showdown from "showdown";
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.use("/entries", express.static(path.resolve(__dirname, "../entries")));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("hej hej");
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
-// MORE ROUTES HERE
+app.get("/entries/:fileName", (req: Request, res: Response) => {
+  const { fileName } = req.params;
+  const filePath = path.resolve(__dirname, "../entries", `${fileName}.md`);
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+
+    const converter = new showdown.Converter();
+    const htmlContent = converter.makeHtml(data);
+
+    res.send(`
+      <html>
+        <head><title>${fileName}</title></head>
+        <body>
+          <h1>${fileName}</h1>
+          <div>${htmlContent}</div>
+          <a href="/">Back to all posts</a>
+        </body>
+      </html>
+    `);
+  });
+});
 
 export default app;
