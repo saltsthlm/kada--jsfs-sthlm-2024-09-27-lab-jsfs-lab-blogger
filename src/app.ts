06ceb383bbe7646079782245b3b6e9a4deaf9510
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-29 12:12:04 +0100

    ran-prettier
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import fs from "fs/promises";
 export const app = express();
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('/entries/:fileName', async (req, res) => {
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/entries/:fileName", async (req, res) => {
   try {
     const fileName = req.params.fileName;
-    const filePath = path.join(__dirname, '../entries', fileName);
-    const data = await fs.readFile(filePath, 'utf-8');
+    const filePath = path.join(__dirname, "../entries", fileName);
+    const data = await fs.readFile(filePath, "utf-8");
     res.json({ content: data });
   } catch (error) {
-    res.status(404).send('File not found');
+    res.status(404).send("File not found");
   }
 });
