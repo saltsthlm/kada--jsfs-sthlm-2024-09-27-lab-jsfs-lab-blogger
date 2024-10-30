76f27f55683e7b601f0403891d5376e3ea647a8b
CarlUdo <carl@wbsweden.com>
2024-10-30 06:15:58 +0100

    Update-test-for-jsdom
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app.test.ts
+++ src/app.test.ts
@@ -20,23 +20,27 @@ describe("GET /entries/:fileName", () => {
   });
 });
 
-describe("DOM Testing with jsdom", () => {
+describe("DOM Testing with JSDOM", () => {
   it("should render list items for blog entries", () => {
     const htmlContent = `
-        <html>
-          <body>
-            <ul id="entriesList">
-              <li><a href="/entries/failOnlyOneTest.md<">failOnlyOneTest.md</a></li>
-              <li><a href="/entries/saveyourfingers-1.md">saveyourfingers-1.md</a></li>
-            </ul>
-          </body>
-        </html>
-      `;
-
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Salt Blogger</title>
+      </head>
+      <body style="padding: 2rem 4rem">
+        <h1 style="margin-bottom: 3rem">Salt Blogger</h1>
+        <ul id="entriesList">
+          <li><a href="#" onclick="fetchMarkdown('failOnlyOneTest.md')">failOnlyOneTest.md</a></li>
+          <li><a href="#" onclick="fetchMarkdown('saveyourfingers-1.md')">saveyourfingers-1.md</a></li>
+        </ul>
+      </body>
+      </html>
+    `;
     const dom = new JSDOM(htmlContent);
     const document = dom.window.document;
     const entriesList = document.getElementById("entriesList");
-
+    
     expect(entriesList).not.toBeNull();
     expect(entriesList?.children.length).toBe(2);
     expect(entriesList?.children[0].textContent).toBe("failOnlyOneTest.md");
